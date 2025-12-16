a4e8e3dab2ad9082dec66b058c03a9de95b977da
LeoWang-LW <yuzhewang2017@gmail.com>
2025-12-16 17:48:09 +0000

    12/16/25 Started

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -6,5 +6,26 @@ import java.util.LinkedList;
 class StackQueueMini {
   public static void main(String[] args) {
 
+    Scanner in = new Scanner(System.in); 
+    int count = 0;
+    Stack<Integer> myStack = new Stack<Integer>(); 
+
+    while(count < 5){
+    System.out.println("Please enter a whole number: ");
+    int number = in.nextInt(); 
+    myStack.push(number); 
+    count++; 
+    }
+
+    System.out.println(myStack); 
+
+
   } // end main
+
+  public static int duplicate(Stack <Integer> e){
+    Stack <Integer> temp = e;
+    Stack <Integer> temp2 = e; 
+    Stack <Integer> temp3 = new Stack<Integer>(); 
+    
+  }
 } // end class
